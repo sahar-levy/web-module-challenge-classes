b6b219be02c8f119d242b925bcef36788dac0878
Sahar Levy <sahar@nogalesfundmgmt.com>
2023-05-06 12:44:24 -0700

    Created a Person class and added methods to the prototype

--- index.js
+++ index.js
@@ -44,8 +44,33 @@ class Airplane {
 */
 
 class Person {
-  
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+
+  eat(edible){
+    if(this.stomach.length < 10){
+      this.stomach.push(edible);
+    };
+  }
+
+  poop(){
+    this.stomach = [];
+  }
+
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
 }
+
+const mary = new Person('Mary', 50);
+console.log(mary.toString());
+mary.eat('taco');
+console.log(mary.stomach);
+mary.poop();
+console.log(mary.stomach);
 
 /*
   TASK 2
