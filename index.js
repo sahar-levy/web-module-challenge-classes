4f7aadc8b1713727a5261a54b534e2f600e6e4de
Sahar Levy <sahar@nogalesfundmgmt.com>
2023-05-09 11:29:45 -0700

    Created Lambdasian class with speak method

--- index.js
+++ index.js
@@ -132,7 +132,15 @@ console.log(myCar);
 */
 
 class Lambdasian {
-  
+  constructor({name, age, location}){
+    this.name = name;
+    this.age = age;
+    this.location = location;
+  }
+
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
